1304710a114906d5182cc4b9a4ae26f47b430076
Woody Liu <bestlifeforzhi@gmail.com>
2021-11-24 21:23:11 +0800

    feat: viewController type rebuild to SectionType and RowType

--- ObservedTry/ViewController/ViewController.swift
+++ ObservedTry/ViewController/ViewController.swift
@@ -7,18 +7,59 @@
 
 import UIKit
 
+typealias TypeNeed = RowType & CustomStringConvertible
+protocol RowType {
+    var viewController: UIViewController { get }
+    static var count: Int { get }
+}
+
 class ViewController: UIViewController {
     
-    enum RowType: Int, CaseIterable, CustomStringConvertible {
+    enum SectionType: Int, CaseIterable, CustomStringConvertible {
+
+        case single, multitude
         
-        case old, withDataMethod, observed
+        var description: String {
+            switch self {
+            case .single:
+                return "One To One"
+            case .multitude:
+                return "One To Many"
+            }
+        }
+        
+        func getRowType(_ row: Int)-> TypeNeed? {
+            switch self {
+            case .single:
+                return SingleType.init(rawValue: row)
+            case .multitude:
+                return MultiType.init(rawValue: row)
+            }
+        }
+        
+        var rowCount: Int {
+            switch self {
+            case .single:
+                return SingleType.allCases.count
+            case .multitude:
+                return MultiType.allCases.count
+            }
+        }
+        
+    }
+    
+    enum SingleType: Int, TypeNeed, CaseIterable {
+        
+        case old
+//        case withDataMethod
+        case observed
         
         var viewController: UIViewController {
             switch self {
             case .old:
                 return OldViewController()
-            case .withDataMethod:
-                return WithDataMethodViewController()
+//            case .withDataMethod:
+//                return WithDataMethodViewController()
             case .observed:
                 return ObserverdViewController()
             }
@@ -28,11 +69,42 @@ class ViewController: UIViewController {
             switch self {
             case .old:
                 return "old"
-            case .withDataMethod:
-                return "withDataMethod"
+//            case .withDataMethod:
+//                return "withDataMethod"
             case .observed:
                 return "observed"
             }
+        }
+        
+        static var count: Int {
+            return SingleType.allCases.count
+        }
+    }
+    
+    enum MultiType: Int, TypeNeed, CaseIterable {
+    
+        case old, observed
+        
+        var viewController: UIViewController {
+            switch self {
+            case .old:
+                return OldViewController()
+            case .observed:
+                return ObserverdViewController()
+            }
+        }
+        
+        var description: String {
+            switch self {
+            case .old:
+                return "old"
+            case .observed:
+                return "observed"
+            }
+        }
+        
+        static var count: Int {
+            return MultiType.allCases.count
         }
         
     }
@@ -58,13 +130,26 @@ class ViewController: UIViewController {
 // MARK: UITableViewDelegate
 extension ViewController: UITableViewDelegate {
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return SectionType.allCases.count
+    }
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 60
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        guard let type = RowType.init(rawValue: indexPath.row) else { return }
+        guard let sectionType = SectionType.init(rawValue: indexPath.section),
+              let type = sectionType.getRowType(indexPath.row) else { return }
         navigationController?.pushViewController(type.viewController, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return SectionType.init(rawValue: section)?.description
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 44
     }
 }
 
@@ -72,13 +157,19 @@ extension ViewController: UITableViewDelegate {
 extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return RowType.allCases.count
+        guard let sectionType = SectionType.init(rawValue: section) else {
+            fatalError("Missing Section Type")
+        }
+        return sectionType.rowCount
     }
 
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: UITableViewCell.description(), for: indexPath)
-        guard let type = RowType.init(rawValue: indexPath.row) else { fatalError("Row is Out of range") }
+        guard let sectionType = SectionType.init(rawValue: indexPath.section),
+              let type = sectionType.getRowType(indexPath.row) else {
+                  fatalError("Missing Row Type / Section Type")
+              }
         cell.textLabel?.text = type.description
         cell.textLabel?.font = .systemFont(ofSize: 25)
         cell.textLabel?.textColor = .blue
